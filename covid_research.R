6ec31afe1edfda9a9bfa9c3324ca88f621af8bdf
Ryan Beck <rb200@humboldt.edu>
2021-02-06 00:07:14 -0800

    Changed the dates to the Date attribute format for R, and added a tiny bit of code trying to subset my csv file into single days and not a 7 month chunk.

--- covid_research.R
+++ covid_research.R
@@ -12,11 +12,15 @@ help("mapper1D")
 ##Make sure that you replace what is in the "double quotes" on the next line with the file path that you have this
 ##R-Script saved and all of the .csv files as well.
 
-setwd("C:/Users/Paral/Documents/School/CURM/Covid Info")
+setwd("C:/Users/Paral/Documents/School/CURM/Covid-19_Research_Data")
 library(TDAmapper)
 require(fastcluster)
 library(igraph)
 
 rm(list = ls())
 
-basedata<-read.csv('national-ICU.csv')
+basedata<-read.csv('Covid-19 Data-Set.csv')
+
+class(basedata$collection_date)
+
+mydata<-subset(basedata,collection_date='7/1/2020')
