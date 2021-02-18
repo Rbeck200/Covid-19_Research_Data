ee5aed23f4b83a11a3f870033b2434b5c7cfa759
Ryan Beck <rb200@humboldt.edu>
2021-02-17 16:46:02 -0800

    upload pics

--- covid_research.R
+++ covid_research.R
@@ -39,20 +39,20 @@ plot(ICU.Beds.Occupied.Estimated~collection_date, data=caldata)
 ##SOmething like for(int i; i < month.length; i++) so that I can loo through a month at a time
 #########-=--------------------------------------------------------------------------------------------------------
 
-ggplot(data = caldata, aes(x = collection_date, y = ICU.Beds.Occupied.Estimated)) +
-  geom_point() +
-  geom_path()+
+ggplot(data = caldata, aes(x = collection_date )) +
+  geom_path(aes(y = ICU.Beds.Occupied.Estimated), color = "blue")+
   labs(x = "Date",
        y = "Total ICU Bed Occupation",
        title = "ICU Occupation - California",
-       subtitle = "7/2020 - 1/2021")
+       subtitle = "7/2020 - 1/2021")+
+       scale_x_date(date_labels = "%B",date_breaks = "1 month")+
+       theme(axis.text.x = element_text(angle=45, hjust = 1))
 
-ggplot(data = caldata, aes(x = collection_date, y = ((Deaths/calPopValue)*10000))) +
-  geom_point() +
-  geom_path()+
+ggplot(data = caldata, aes(x = collection_date )) +
+  geom_path(aes(y = ((Deaths/calPopValue)*100000)), color = "darkred")+
   labs(x = "Date",
-       y = "Deaths per 10,000 people",
-       title = "Deaths per 10,000 people - California",
+       y = "Deaths per 100,000 people",
+       title = "Deaths per 100,000 people - California",
        subtitle = "7/2020 - 1/2021")
 
 
@@ -62,23 +62,23 @@ dist_mat_ICU<-as.matrix(dist(ICUdf))
 write.csv(dist_mat_ICU,'distICU.csv')
 rm(dist_mat_ICU)
   
-DEATHdf<-data.frame(x=caldata[,2], y=((caldata[,12]/calPopValue)*10000))
+DEATHdf<-data.frame(x=caldata[,2], y=((caldata[,12]/calPopValue)*100000))
 
 dist_mat_DEATH<-as.matrix(dist(DEATHdf))
 write.csv(dist_mat_DEATH,'distDEATH.csv')
 rm(dist_mat_DEATH)
 
-threeDdf<-data.frame(x=caldata[,2], y=caldata[,3], z=((caldata[,12]/calPopValue)*10000))
+threeDdf<-data.frame(x=caldata[,2], y=caldata[,3], z=((caldata[,12]/calPopValue)*100000))
 
 m1 <- mapper1D( 
   distance_matrix = dist(ICUdf), 
   filter_values = ICUdf[,2], 
-  num_intervals = 8,
-  percent_overlap = 50, 
-  num_bins_when_clustering = 20) 
+  num_intervals = 10,
+  percent_overlap = 26, 
+  num_bins_when_clustering = 8) 
 
 g1 <- graph.adjacency(m1$adjacency, mode="undirected")
-plot(g1, layout = layout.auto(g1) ,xlab='X',ylab='Y',main='Dist - 1')
+plot(g1, layout = layout.auto(g1) ,xlab='Date',ylab='Staffed Occupied ICU Beds',main='ICU Occupation - 7/20-1/21')
 
 
 
@@ -87,10 +87,10 @@ m2 <- mapper1D(
   filter_values = DEATHdf[,2],  
   num_intervals = 8,
   percent_overlap = 50, 
-  num_bins_when_clustering = 20) 
+  num_bins_when_clustering = 10) 
 
 g2 <- graph.adjacency(m2$adjacency, mode="undirected")
-plot(g2, layout = layout.auto(g2) ,xlab='X',ylab='Y',main='Death Rate per 10000')
+plot(g2, layout = layout.auto(g2) ,xlab='X',ylab='Y',main='Death Rate per 100000')
 
-##m3 <- mapper3D()
+##m3 <- mapper()
 
